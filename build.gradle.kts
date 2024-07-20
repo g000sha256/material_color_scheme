6e6b5b94df805380076f507539400b9e254cfb90
g000sha256 <detmmpmznb@g000sha256.dev>
2024-07-20 19:42:23 +0300

    Update build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,24 @@
+import g000sha256.sonatype_maven_central.SonatypeMavenCentralType
+import g000sha256.sonatype_maven_central.sonatypeMavenCentralRepository
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import proguard.gradle.ProGuardTask
 
-val libraryGroup = "com.github.g000sha256"
-val libraryModule = "material_color_scheme"
-val libraryVersion = "1.1.0"
-
-group = libraryGroup
-version = libraryVersion
+val libraryGroup = "dev.g000sha256"
+val libraryModule = "material-color-scheme"
+val libraryVersion = "1.2.0"
 
 buildscript {
-    dependencies { classpath(catalog.plugin.proguard) }
+    dependencies {
+        classpath(catalog.plugin.proguard)
+        classpath(catalog.plugin.sonatype)
+    }
 }
 
 plugins {
+    alias(catalog.plugins.jetbrains.dokka)
     alias(catalog.plugins.jetbrains.kotlin.jvm)
     id("org.gradle.maven-publish")
+    id("org.gradle.signing")
 }
 
 java {
@@ -23,8 +27,6 @@ java {
 }
 
 kotlin {
-    jvmToolchain(8)
-
     compilerOptions {
         jvmTarget = JvmTarget.JVM_1_8
         moduleName = "g000sha256.material.color_scheme"
@@ -80,6 +82,17 @@ val minifyTaskProvider = tasks.register<ProGuardTask>("minify") {
 
 val sourcesJarTaskProvider = tasks.kotlinSourcesJar
 
+val dokkaJavaDocTaskProvider = tasks.dokkaJavadoc
+
+val dokkaJavaDocJarTaskProvider = tasks.register<Jar>("dokkaJavaDocJar") {
+    archiveClassifier = "javadoc"
+    group = "documentation"
+    dependsOn(dokkaJavaDocTaskProvider)
+
+    val dokkaJavaDocTask = dokkaJavaDocTaskProvider.get()
+    from(dokkaJavaDocTask.outputDirectory)
+}
+
 publishing {
     publications {
         register<MavenPublication>("release") {
@@ -91,6 +104,7 @@ publishing {
                 name = "Material Color Scheme"
                 description = "Dynamic Material 3 color scheme builder for Compose"
                 url = "https://github.com/g000sha256/material_color_scheme"
+                inceptionYear = "2024"
 
                 licenses {
                     license {
@@ -103,15 +117,20 @@ publishing {
                     developer {
                         id = "g000sha256"
                         name = "Georgii Ippolitov"
-                        email = "material_color_scheme.github@g0sha.dev"
+                        email = "detmmpmznb@g000sha256.dev"
                         url = "https://github.com/g000sha256"
                     }
                 }
 
                 scm {
                     connection = "scm:git:git://github.com/g000sha256/material_color_scheme.git"
-                    developerConnection = "scm:git:ssh://github.com/g000sha256/material_color_scheme.git"
-                    url = "https://github.com/g000sha256/material_color_scheme"
+                    developerConnection = "scm:git:ssh://github.com:g000sha256/material_color_scheme.git"
+                    url = "https://github.com/g000sha256/material_color_scheme/tree/master"
+                }
+
+                issueManagement {
+                    system = "GitHub Issues"
+                    url = "https://github.com/g000sha256/material_color_scheme/issues"
                 }
 
                 withXml {
@@ -133,6 +152,34 @@ publishing {
             artifact(minifyJar) { builtBy(minifyTaskProvider) }
 
             artifact(sourcesJarTaskProvider) { builtBy(sourcesJarTaskProvider) }
+
+            artifact(dokkaJavaDocJarTaskProvider) { builtBy(dokkaJavaDocJarTaskProvider) }
         }
     }
+}
+
+signing {
+    val key = getProperty("Signing.Key") ?: getEnvironment("SIGNING_KEY")
+    val password = getProperty("Signing.Password") ?: getEnvironment("SIGNING_PASSWORD")
+    useInMemoryPgpKeys(key, password)
+
+    val publication = publishing.publications["release"]
+    sign(publication)
+}
+
+sonatypeMavenCentralRepository {
+    type = SonatypeMavenCentralType.Manual
+
+    credentials {
+        username = getProperty("SonatypeMavenCentral.Username") ?: getEnvironment("SONATYPE_USERNAME")
+        password = getProperty("SonatypeMavenCentral.Password") ?: getEnvironment("SONATYPE_PASSWORD")
+    }
+}
+
+private fun getProperty(key: String): String? {
+    return properties[key] as String?
+}
+
+private fun getEnvironment(key: String): String? {
+    return System.getenv(key)
 }
