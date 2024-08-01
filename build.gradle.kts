c50313ef5f0bf5c313efbd53cd87a3e8b8db503a
g000sha256 <detmmpmznb@g000sha256.dev>
2024-08-01 18:45:28 +0300

    Use multiplatform instead of jvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 import g000sha256.sonatype_maven_central.SonatypeMavenCentralType
 import g000sha256.sonatype_maven_central.sonatypeMavenCentralRepository
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+
+val projectName = name
 
 val libraryGroup = "dev.g000sha256"
 val libraryModule = "material-color-scheme"
@@ -11,10 +15,26 @@ buildscript {
 }
 
 plugins {
+    alias(catalog.plugins.android.library)
     alias(catalog.plugins.jetbrains.dokka)
-    alias(catalog.plugins.jetbrains.kotlin.jvm)
+    alias(catalog.plugins.jetbrains.kotlin.multiplatform)
     id("org.gradle.maven-publish")
     id("org.gradle.signing")
+}
+
+val kotlinModuleName = "g000sha256.material.color_scheme"
+val androidNamespace = kotlinModuleName
+
+android {
+    compileSdk = 34
+    namespace = kotlinModuleName
+
+    defaultConfig { minSdk = 21 }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 java {
@@ -23,13 +43,40 @@ java {
 }
 
 kotlin {
-    compilerOptions {
-        jvmTarget = JvmTarget.JVM_1_8
-        moduleName = "g000sha256.material.color_scheme"
+    withSourcesJar(publish = true)
+
+    androidTarget {
+        publishLibraryVariants("release")
+
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_1_8
+            moduleName = kotlinModuleName
+        }
     }
 
+    jvm {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_1_8
+            moduleName = kotlinModuleName
+        }
+    }
+
+    js(IR) { browser() }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs { browser() }
+
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+
+    macosArm64()
+    macosX64()
+
     sourceSets {
-        main {
+        commonMain {
             dependencies {
                 implementation(catalog.library.jetbrains.annotations)
                 implementation(catalog.library.jetbrains.kotlin)
@@ -40,15 +87,13 @@ kotlin {
             }
         }
 
-        test {
+        commonTest {
             dependencies { implementation(catalog.test.kotlin) }
         }
     }
 }
 
-val sourcesJarTaskProvider = tasks.kotlinSourcesJar
-
-val dokkaJavaDocTaskProvider = tasks.dokkaJavadoc
+val dokkaJavaDocTaskProvider = tasks.dokkaHtml
 
 val dokkaJavaDocJarTaskProvider = tasks.register<Jar>("dokkaJavaDocJar") {
     archiveClassifier = "javadoc"
@@ -61,10 +106,11 @@ val dokkaJavaDocJarTaskProvider = tasks.register<Jar>("dokkaJavaDocJar") {
 
 publishing {
     publications {
-        register<MavenPublication>("release") {
+        withType<MavenPublication> {
             groupId = libraryGroup
-            artifactId = libraryModule
             version = libraryVersion
+
+            afterEvaluate { artifactId = artifactId.replace(projectName, libraryModule) }
 
             pom {
                 name = "Material Color Scheme"
@@ -100,10 +146,6 @@ publishing {
                 }
             }
 
-            val component = components["kotlin"]
-            from(component)
-
-            artifact(sourcesJarTaskProvider)
             artifact(dokkaJavaDocJarTaskProvider)
         }
     }
@@ -114,8 +156,27 @@ signing {
     val password = getProperty("Signing.Password") ?: getEnvironment("SIGNING_PASSWORD")
     useInMemoryPgpKeys(key, password)
 
-    val publication = publishing.publications["release"]
-    sign(publication)
+    sign(publishing.publications)
+}
+
+tasks.withType<Sign> {
+    signatureType = object : AbstractSignatureType(), SignatureType by signatureType {
+
+        override fun fileFor(toSign: File): File {
+            val original = super.fileFor(toSign)
+            return layout
+                .buildDirectory
+                .dir("signatures/$name")
+                .get()
+                .file(original.name)
+                .asFile
+        }
+
+        override fun sign(signatory: Signatory, toSign: File): File {
+            return super.sign(signatory, toSign)
+        }
+
+    }
 }
 
 sonatypeMavenCentralRepository {
